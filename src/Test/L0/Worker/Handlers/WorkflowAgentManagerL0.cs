1f03210a5d403eb2fd74325a80b0a3407b60b0c9
quoctruong <quoct@google.com>
2026-06-17 18:07:16 +0000

    Implement bidirectional file command synchronization in WorkflowAgentManager

--- src/Test/L0/Worker/Handlers/WorkflowAgentManagerL0.cs
+++ src/Test/L0/Worker/Handlers/WorkflowAgentManagerL0.cs
@@ -39,5 +39,35 @@ namespace GitHub.Runner.Common.Tests.Worker.Handlers
                 await manager.SyncDirectoryToWorkflowPodAsync(ec.Object, "somedir");
             }
         }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public void InitializeFileCommand_ReturnsEarlyWhenNoSharedVolumeDisabled()
+        {
+            using (var hostContext = new TestHostContext(this))
+            {
+                var manager = new WorkflowAgentManager();
+                manager.Initialize(hostContext);
+
+                var ec = new Mock<IExecutionContext>();
+                manager.InitializeFileCommand(ec.Object, null, "somepath", "context");
+            }
+        }
+
+        [Fact]
+        [Trait("Level", "L0")]
+        [Trait("Category", "Worker")]
+        public async Task SyncFileCommandsFromWorkflowPodAsync_ReturnsEarlyWhenNoSharedVolumeDisabled()
+        {
+            using (var hostContext = new TestHostContext(this))
+            {
+                var manager = new WorkflowAgentManager();
+                manager.Initialize(hostContext);
+
+                var ec = new Mock<IExecutionContext>();
+                await manager.SyncFileCommandsFromWorkflowPodAsync(ec.Object, null, "dir", "suffix", null);
+            }
+        }
     }
 }
